6004e12d890eb653fef05f8d600a09d543c1a033
cevap <dev@i2pmail.org>
2019-03-16 05:39:32 +0100

    Replace lint checks with latest bitcoins and add scripts to test folder

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -10,9 +10,12 @@ export LC_ALL=C
 #contrib/devtools/git-subtree-check.sh src/secp256k1
 #contrib/devtools/git-subtree-check.sh src/univalue
 #contrib/devtools/git-subtree-check.sh src/leveldb
-contrib/devtools/check-doc.py
-contrib/devtools/logprint-scanner.py
+test/lint/check-doc.py
+test/lint/check-rpc-mappings.py .
+test/lint/lint-all.sh
 
 if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
-  contrib/devtools/lint-whitespace.sh
+    git log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
+    while read -r LINE; do travis_retry gpg --keyserver hkp://subset.pool.sks-keyservers.net --recv-keys $LINE; done < contrib/verify-commits/trusted-keys &&
+    travis_wait 50 contrib/verify-commits/verify-commits.py --clean-merge=2;
 fi
