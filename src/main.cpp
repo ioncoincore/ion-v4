54d936fd0a67751f7add181f5c5e2c72678af4a6
FornaxA <fornaxa@servitising.org>
2019-03-20 13:25:42 +0100

    Refactor: separate validation from writing to memory when adding token groups

--- src/main.cpp
+++ src/main.cpp
@@ -3418,7 +3418,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                     }
                 }
                 CTokenGroupCreation newTokenGroupCreation;
-                if (tokenGroupManager->AddTokenGroup(tx, newTokenGroupCreation)) {
+                if (tokenGroupManager->CreateTokenGroup(tx, newTokenGroupCreation)) {
                     newTokenGroups.push_back(newTokenGroupCreation);
                 } else {
                     return state.Invalid(false, REJECT_INVALID, "bad OP_GROUP");
@@ -3608,6 +3608,9 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
 
     if (!pTokenDB->WriteTokenGroupsBatch(newTokenGroups))
         return AbortNode(state, "Failed to write token creation data");
+    if (!tokenGroupManager->AddTokenGroups(newTokenGroups)) {
+        return AbortNode(state, "Failed to add token creation data");
+    }
 
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
