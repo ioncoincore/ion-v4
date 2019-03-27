b4729036e52e3b0503ac9fd36d382a7cbea480b7
FornaxA <fornaxa@servitising.org>
2019-03-27 20:14:47 +0100

    Apply the DarkMatter fee structure
    Creating and minting regular transactions requires a fee in DarkMatter
    Sending DarkMatter requires a fee (in DarkMatter) too

--- src/main.cpp
+++ src/main.cpp
@@ -2641,10 +2641,19 @@ bool CheckInputs(const CTransaction& tx, CValidationState& state, const CCoinsVi
                                           tx.GetHash().ToString(), FormatMoney(nValueIn), FormatMoney(tx.GetValueOut())),
                     REJECT_INVALID, "bad-txns-in-belowout");
 
-            if (((int)chainActive.Tip()->nHeight >= Params().OpGroup_StartHeight()) &&
-                !CheckTokenGroups(tx, state, inputs, tgMintMeltBalance))
-            {
-                return state.DoS(0, error("Token group inputs and outputs do not balance"), REJECT_MALFORMED, "token-group-imbalance");
+            if ((int)chainActive.Tip()->nHeight >= Params().OpGroup_StartHeight()) {
+                if (!CheckTokenGroups(tx, state, inputs, tgMintMeltBalance))
+                    return state.DoS(0, error("Token group inputs and outputs do not balance"), REJECT_MALFORMED, "token-group-imbalance");
+
+                //Check that all token transactions paid their XDM fees
+                CAmount nXDMFees = 0;
+                if (!fVerifyingBlocks) {
+                    if (!tokenGroupManager->CheckXDMFees(tx, tgMintMeltBalance, state, pindexPrev, nXDMFees)) {
+                        return state.DoS(0, error("Token transaction does not pay enough XDM fees"), REJECT_MALFORMED, "token-group-imbalance");
+                    }
+                } else {
+                    LogPrint("token", "%s - XDM fee payment check skipped on sync\n", __func__);
+                }
             }
 
             // Tally transaction fees
