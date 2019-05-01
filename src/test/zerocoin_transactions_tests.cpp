6e5f498b55dcabddb02c7977c4108cea237732e0
furszy <matiasfurszyfer@protonmail.com>
2019-05-01 11:47:18 +0200

    public coin spend creation connected + first mem pool validation.
    [Zerocoin] public coin spend improvements.
    
    [Zerocoin] publicCoinSpend validations connected to the accept txes flow.
    
    [Zerocoin] public spends not checked as regular inputs.
    
    [Zerocoin] publicCoinSpend validation as regular spends.
    
    non good solution for the coinSpend pointer inheritance issue but at least working
    
    [Zerocoin] ParseZerocoinPublicSpend moved into xionmodule file.
    
    cleanup unused field.

--- src/test/zerocoin_transactions_tests.cpp
+++ src/test/zerocoin_transactions_tests.cpp
@@ -61,8 +61,6 @@ BOOST_AUTO_TEST_CASE(zerocoin_public_spend_test)
     ZerocoinParams *ZCParams = Params().Zerocoin_Params(false);
     (void)ZCParams;
 
-    XIONModule xionModule;
-
     PrivateCoin privCoin(ZCParams, libzerocoin::CoinDenomination::ZQ_ONE, true);
     const CPrivKey privKey = privCoin.getPrivKey();
 
@@ -95,17 +93,17 @@ BOOST_AUTO_TEST_CASE(zerocoin_public_spend_test)
     tx.vout[0].scriptPubKey = GetScriptForDestination(CBitcoinAddress("D9Ti4LEhF1n6dR2hGd2SyNADD51AVgva6q").Get());
 
     CTxIn in;
-    if (!xionModule.createInput(in, mint, tx.GetHash())){
+    if (!XIONModule::createInput(in, mint, tx.GetHash())){
         BOOST_CHECK_MESSAGE(false, "Failed to create zc input");
     }
 
     PublicCoinSpend publicSpend(ZCParams);
-    if (!xionModule.validateInput(in, out, tx, publicSpend)){
+    if (!XIONModule::validateInput(in, out, tx, publicSpend)){
         BOOST_CHECK_MESSAGE(false, "Failed to validate zc input");
     }
 
     PublicCoinSpend publicSpendTest(ZCParams);
-    BOOST_CHECK_MESSAGE(xionModule.parseCoinSpend(in, tx, out, publicSpendTest), "Failed to parse public spend");
+    BOOST_CHECK_MESSAGE(XIONModule::parseCoinSpend(in, tx, out, publicSpendTest), "Failed to parse public spend");
     libzerocoin::CoinSpend *spend = &publicSpendTest;
 
     BOOST_CHECK_MESSAGE(publicSpendTest.HasValidSignature(), "Failed to validate public spend signature");
