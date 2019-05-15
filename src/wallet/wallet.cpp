49ba32c53e9fb5f42e8cb51fa992de6942ea6097
random-zebra <random.zebra@protonmail.com>
2019-05-15 05:29:22 +0200

    Disable zerocoins inclusion in 'SelectStakeCoins'

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2150,6 +2150,7 @@ bool CWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInp
         }
     }
 
+    /* Disable xION Staking
     //xION
     if ((GetBoolArg("-xionstake", true) || fPrecompute) && chainActive.Height() > Params().Zerocoin_Block_V2_Start() && !IsSporkActive(SPORK_9_ZEROCOIN_MAINTENANCE_MODE)) {
         //Only update xION set once per update interval
@@ -2179,7 +2180,7 @@ bool CWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInp
             }
         }
     }
-
+    */
     return true;
 }
 
