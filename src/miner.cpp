f3095124df8299f45a62a8f92024de2943a87800
FornaxA <fornaxa@servitising.org>
2019-06-06 13:17:37 +0200

    Update PoW functionality for mining on regtest
    See also https://github.com/ioncoincore/ion/commit/6de5fc9694899638b6810e14ad05222578aa8c09

--- src/miner.cpp
+++ src/miner.cpp
@@ -490,7 +490,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         //Calculate the accumulator checkpoint only if the previous cached checkpoint need to be updated
         if (fZerocoinActive) {
             uint256 nCheckpoint;
-            uint256 hashBlockLastAccumulated = chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash();
+            uint256 hashBlockLastAccumulated = nHeight >= 20 ? chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash(): 0;
             if (nHeight >= pCheckpointCache.first || pCheckpointCache.second.first != hashBlockLastAccumulated) {
                 //For the period before v2 activation, xION will be disabled and previous block's checkpoint is all that will be needed
                 pCheckpointCache.second.second = pindexPrev->nAccumulatorCheckpoint;
