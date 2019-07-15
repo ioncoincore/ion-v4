nxboost/nxboost

Valid blocks from forks badly rejected due an invalid view of the available utxo set for forked chains + split height going one block further than what should be.
[Net] acceptBlock, back to chain split was going one block further if prev was the previous block of the incoming block.

[Net] coins cache view only has the tip view and not forks utxo chain view.

src/main.cpp
@@ -4350,17 +4350,17 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         CTransaction &stakeTxIn = block.vtx[1];
 
         // Inputs
-        std::vector<CTxIn> NXBInputs;
+        std::vector<CTxIn> nxbInputs;
         std::vector<CTxIn> zNXBInputs;
 
         for (const CTxIn& stakeIn : stakeTxIn.vin) {
             if(stakeIn.scriptSig.IsZerocoinSpend()){
                 zNXBInputs.push_back(stakeIn);
             }else{
-                NXBInputs.push_back(stakeIn);
+                nxbInputs.push_back(stakeIn);
             }
         }
-        const bool hasNXBInputs = !NXBInputs.empty();
+        const bool hasNXBInputs = !nxbInputs.empty();
         const bool hasZNXBInputs = !zNXBInputs.empty();
 
         // ZC started after PoS.
@@ -4383,7 +4383,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                 if(tx.IsCoinStake()) continue;
                 if(hasNXBInputs)
                     // Check if coinstake input is double spent inside the same block
-                    for (const CTxIn& NXBIn : NXBInputs){
+                    for (const CTxIn& NXBIn : nxbInputs){
                         if(NXBIn.prevout == in.prevout){
                             // double spent coinstake input inside block
                             return error("%s: double spent coinstake input inside block", __func__);
@@ -4400,47 +4400,50 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             // Start at the block we're adding on to
             CBlockIndex *prev = pindexPrev;
 
-            int readBlock = 0;
             vector<CBigNum> vBlockSerials;
-            CBlock bl;
-            // Go backwards on the forked chain up to the split
-            do {
-                // Check if the forked chain is longer than the max reorg limit
-                if(readBlock == Params().MaxReorganizationDepth()){
-                    // TODO: Remove this chain from disk.
-                    return error("%s: forked chain longer than maximum reorg limit", __func__);
-                }
+            if (!chainActive.Contains(prev)) {
+                int readBlock = 0;
+                CBlock bl;
+                // Go backwards on the forked chain up to the split
+                do {
+                    // Check if the forked chain is longer than the max reorg limit
+                    if (readBlock == Params().MaxReorganizationDepth()) {
+                        // TODO: Remove this chain from disk.
+                        return error("%s: forked chain longer than maximum reorg limit", __func__);
+                    }
 
-                if(!ReadBlockFromDisk(bl, prev))
-                    // Previous block not on disk
-                    return error("%s: previous block %s not on disk", __func__, prev->GetBlockHash().GetHex());
-                // Increase amount of read blocks
-                readBlock++;
-                // Loop through every input from said block
-                for (const CTransaction& t : bl.vtx) {
-                    for (const CTxIn& in: t.vin) {
-                        // Loop through every input of the staking tx
-                        for (const CTxIn& stakeIn : NXBInputs) {
-                            // if it's already spent
+                    if (!ReadBlockFromDisk(bl, prev))
+                        // Previous block not on disk
+                        return error("%s: previous block %s not on disk", __func__, prev->GetBlockHash().GetHex());
+                    // Increase amount of read blocks
+                    readBlock++;
+                    // Loop through every input from said block
+                    for (const CTransaction &t : bl.vtx) {
+                        for (const CTxIn &in: t.vin) {
+                            // Loop through every input of the staking tx
+                            for (const CTxIn &stakeIn : nxbInputs) {
+                                // if it's already spent
 
-                            // First regular staking check
-                            if(hasNXBInputs) {
-                                if (stakeIn.prevout == in.prevout) {
-                                    return state.DoS(100, error("%s: input already spent on a previous block", __func__));
-                                }
+                                // First regular staking check
+                                if (hasNXBInputs) {
+                                    if (stakeIn.prevout == in.prevout) {
+                                        return state.DoS(100, error("%s: input already spent on a previous block",
+                                                                    __func__));
+                                    }
 
-                                // Second, if there is zPoS staking then store the serials for later check
-                                if(in.scriptSig.IsZerocoinSpend()){
-                                    vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
+                                    // Second, if there is zPoS staking then store the serials for later check
+                                    if (in.scriptSig.IsZerocoinSpend()) {
+                                        vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                prev = prev->pprev;
+                    prev = prev->pprev;
 
-            } while (!chainActive.Contains(prev));
+                } while (!chainActive.Contains(prev));
+            }
 
             // Split height
             splitHeight = prev->nHeight;
@@ -4502,9 +4505,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     return error("%s: coin stake inputs not available on main chain, received height %d vs current %d", __func__, nHeight, chainActive.Height());
                 }
                 if(coin && !coin->IsAvailable(in.prevout.n)){
-                    // If this is not available get the height of the spent and validate it with the forked height
-                    // Check if this occurred before the chain split
-                    if(!(isBlockFromFork && coin->nHeight > splitHeight)){
+                    if(!isBlockFromFork){
                         // Coins not available
                         return error("%s: coin stake inputs already spent in main chain", __func__);
                     }

ioncoincore/ion-v4

src/main.cpp
@@ -4729,47 +4729,50 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             // Start at the block we're adding on to
             CBlockIndex *prev = pindexPrev;
 
-            int readBlock = 0;
             vector<CBigNum> vBlockSerials;
-            CBlock bl;
-            // Go backwards on the forked chain up to the split
-            do {
-                // Check if the forked chain is longer than the max reorg limit
-                if(readBlock == Params().MaxReorganizationDepth()){
-                    // TODO: Remove this chain from disk.
-                    return error("%s: forked chain longer than maximum reorg limit", __func__);
-                }
+            if (!chainActive.Contains(prev)) {
+                int readBlock = 0;
+                CBlock bl;
+                // Go backwards on the forked chain up to the split
+                do {
+                    // Check if the forked chain is longer than the max reorg limit
+                    if (readBlock == Params().MaxReorganizationDepth()) {
+                        // TODO: Remove this chain from disk.
+                        return error("%s: forked chain longer than maximum reorg limit", __func__);
+                    }
 
-                if(!ReadBlockFromDisk(bl, prev))
-                    // Previous block not on disk
-                    return error("%s: previous block %s not on disk", __func__, prev->GetBlockHash().GetHex());
-                // Increase amount of read blocks
-                readBlock++;
-                // Loop through every input from said block
-                for (const CTransaction& t : bl.vtx) {
-                    for (const CTxIn& in: t.vin) {
-                        // Loop through every input of the staking tx
-                        for (const CTxIn& stakeIn : ionInputs) {
-                            // if it's already spent
+                    if (!ReadBlockFromDisk(bl, prev))
+                        // Previous block not on disk
+                        return error("%s: previous block %s not on disk", __func__, prev->GetBlockHash().GetHex());
+                    // Increase amount of read blocks
+                    readBlock++;
+                    // Loop through every input from said block
+                    for (const CTransaction &t : bl.vtx) {
+                        for (const CTxIn &in: t.vin) {
+                            // Loop through every input of the staking tx
+                            for (const CTxIn &stakeIn : ionInputs) {
+                                // if it's already spent
 
-                            // First regular staking check
-                            if(hasIONInputs) {
-                                if (stakeIn.prevout == in.prevout) {
-                                    return state.DoS(100, error("%s: input already spent on a previous block", __func__));
-                                }
+                                // First regular staking check
+                                if (hasIONInputs) {
+                                    if (stakeIn.prevout == in.prevout) {
+                                        return state.DoS(100, error("%s: input already spent on a previous block",
+                                                                    __func__));
+                                    }
 
-                                // Second, if there is zPoS staking then store the serials for later check
-                                if(in.scriptSig.IsZerocoinSpend()){
-                                    vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
+                                    // Second, if there is zPoS staking then store the serials for later check
+                                    if (in.scriptSig.IsZerocoinSpend()) {
+                                        vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                prev = prev->pprev;
+                    prev = prev->pprev;
 
-            } while (!chainActive.Contains(prev));
+                } while (!chainActive.Contains(prev));
+            }
 
             // Split height
             splitHeight = prev->nHeight;
@@ -4831,9 +4834,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     return error("%s: coin stake inputs not available on main chain, received height %d vs current %d", __func__, nHeight, chainActive.Height());
                 }
                 if(coin && !coin->IsAvailable(in.prevout.n)){
-                    // If this is not available get the height of the spent and validate it with the forked height
-                    // Check if this occurred before the chain split
-                    if(!(isBlockFromFork && coin->nHeight > splitHeight)){
+                    if(!isBlockFromFork){
                         // Coins not available
                         return error("%s: coin stake inputs already spent in main chain", __func__);
                     }