eons-finance/ALQO

fix config.h include file name

src/compat/byteswap.h
@@ -6,7 +6,7 @@
 #define BITCOIN_COMPAT_BYTESWAP_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/pivx-config.h>
 #endif
 
 #include <stdint.h>

src/compat/endian.h
@@ -6,7 +6,7 @@
 #define BITCOIN_COMPAT_ENDIAN_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/pivx-config.h>
 #endif
 
 #include <compat/byteswap.h>

ioncoincore/ion-v4

src/compat/byteswap.h
@@ -6,7 +6,7 @@
 #define BITCOIN_COMPAT_BYTESWAP_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/ion-config.h>
 #endif
 
 #include <stdint.h>

src/compat/endian.h
@@ -6,7 +6,7 @@
 #define BITCOIN_COMPAT_ENDIAN_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/ion-config.h>
 #endif
 
 #include <compat/byteswap.h>