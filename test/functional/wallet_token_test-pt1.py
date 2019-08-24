9dd3a734ab47b438f713b40dd75d9c6391098c95
FornaxA <25762277+FornaxA@users.noreply.github.com>
2019-08-24 21:31:16 +0200

    Fix copyright message

--- test/functional/wallet_token_test-pt1.py
+++ test/functional/wallet_token_test-pt1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2017 The Bitcoin Core developers
-# Copyright (c) 2018-2019 The Ion developers
+# Copyright (c) 2019 The ION Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the functionality of all Token commands.
