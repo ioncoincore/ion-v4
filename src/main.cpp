FornaxA/ion

Token description - second commit
Cleanup and finishing commit 'Token description - unfinished'

src/main.cpp
@@ -3143,15 +3143,21 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                     return state.DoS(100, error("%s : tried to spend invalid input %s in tx %s", __func__, in.prevout.ToString(),
                                   tx.GetHash().GetHex()), REJECT_INVALID, "bad-txns-invalid-inputs");
                 }
-                // Set flag if input is a group token management address
             }
-            //Temporarily disable new token creation during management mode
-            if (block.nTime > GetSporkValue(SPORK_10_TOKENGROUP_MANAGEMENT_MODE) && !IsInitialBlockDownload() && IsAnyTxOutputGroupedCreation(tx)) {
-                if (IsAnyTxOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
-                    LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+            if (IsAnyTxOutputGroupedCreation(tx)) {
+                //Disable new token creation during management mode
+                if (block.nTime > GetSporkValue(SPORK_10_TOKENGROUP_MANAGEMENT_MODE) && !IsInitialBlockDownload()) {
+                    if (IsAnyTxOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+                        LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+                    } else {
+                        return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                                        __func__), REJECT_INVALID, "token-group-management");
+                    }
+                }
+                CTokenGroupCreation newTokenGroupCreation;
+                if (tokenGroupManager->AddTokenGroup(tx, newTokenGroupCreation)) {
                 } else {
-                    return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
-                                    __func__), REJECT_INVALID, "token-group-management");
+                    return state.Invalid(false, REJECT_INVALID, "bad OP_GROUP");
                 }
             }
 
@@ -3193,10 +3199,6 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
             control.Add(vChecks);
         }
         nValueOut += tx.GetValueOut();
-
-        if (IsAnyTxOutputGroupedCreation(tx)) {
-            tokenGroupManager->addNewTokenGroup(tx, state);
-        }
 
         CTxUndo undoDummy;
         if (i > 0) {

ioncoincore/ion-v4

src/main.cpp
@@ -3393,6 +3393,22 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                                   tx.GetHash().GetHex()), REJECT_INVALID, "bad-txns-invalid-inputs");
                 }
             }
+            if (IsAnyTxOutputGroupedCreation(tx)) {
+                //Disable new token creation during management mode
+                if (block.nTime > GetSporkValue(SPORK_10_TOKENGROUP_MAINTENANCE_MODE) && !IsInitialBlockDownload()) {
+                    if (IsAnyTxOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+                        LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+                    } else {
+                        return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                                        __func__), REJECT_INVALID, "token-group-management");
+                    }
+                }
+                CTokenGroupCreation newTokenGroupCreation;
+                if (tokenGroupManager->AddTokenGroup(tx, newTokenGroupCreation)) {
+                } else {
+                    return state.Invalid(false, REJECT_INVALID, "bad OP_GROUP");
+                }
+            }
 
             // Check that xION mints are not already known
             if (tx.HasZerocoinMintOutputs()) {
@@ -3432,10 +3448,6 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
             control.Add(vChecks);
         }
         nValueOut += tx.GetValueOut();
-
-        if (IsAnyTxOutputGroupedCreation(tx)) {
-            tokenGroupManager->addNewTokenGroup(tx, state);
-        }
 
         CTxUndo undoDummy;
         if (i > 0) {