FornaxA/ion

Token group manager: clear token list on reindex

src/tokendb.cpp
@@ -66,6 +66,7 @@ bool ReindexTokenDB(std::string &strError) {
         strError = "Failed to reset token database";
         return false;
     }
+    tokenGroupManager->ClearTokenGroups();
 
     uiInterface.ShowProgress(_("Reindexing token database..."), 0);
 

src/tokengroupmanager.cpp
@@ -134,6 +134,10 @@ bool CTokenGroupManager::AddTokenGroup(CTransaction tx, CTokenGroupCreation &new
     return true;
 }
 
+void CTokenGroupManager::ClearTokenGroups() {
+    mapTokenGroups.clear();
+}
+
 bool CTokenGroupManager::RemoveTokenGroup(CTransaction tx, CTokenGroupID &newTokenGroupID) {
     CScript firstOpReturn;
     CTokenGroupInfo tokenGroupInfo;

ioncoincore/ion-v4

src/tokendb.cpp
@@ -41,6 +41,8 @@ bool CTokenDB::DropTokenGroups() {
 }
 
 bool CTokenDB::LoadTokensFromDB(std::string &strError) {
+    tokenGroupManager->ResetTokenGroups();
+
     boost::scoped_ptr<CLevelDBIterator> pcursor(NewIterator());
 
     pcursor->SeekToFirst();
@@ -66,6 +68,7 @@ bool ReindexTokenDB(std::string &strError) {
         strError = "Failed to reset token database";
         return false;
     }
+    tokenGroupManager->ResetTokenGroups();
 
     uiInterface.ShowProgress(_("Reindexing token database..."), 0);
 

src/tokengroupmanager.cpp
@@ -134,6 +134,10 @@ bool CTokenGroupManager::AddTokenGroup(CTransaction tx, CTokenGroupCreation &new
     return true;
 }
 
+void CTokenGroupManager::ResetTokenGroups() {
+    mapTokenGroups.clear();
+}
+
 bool CTokenGroupManager::RemoveTokenGroup(CTransaction tx, CTokenGroupID &newTokenGroupID) {
     CScript firstOpReturn;
     CTokenGroupInfo tokenGroupInfo;